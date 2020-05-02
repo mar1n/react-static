58b1b75dcb06175182f3ba5be7d8618e98aaa7e3
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-02 20:35:24 +0100

    npm run build and you have seo friendly

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
 import "./styles/App.scss";
-ReactDOM.render(
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>, rootElement);
+} else {
+  render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
